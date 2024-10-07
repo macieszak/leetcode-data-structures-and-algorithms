4b9d3778c8846c10df02592bc4299756c2fd3fbb
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-07 18:25:34 +0200

    Leetcode 853. Car fleet.

---
+++ src/leetcode_problems/stack/car_fleet/CarFleet.java
@@ -0,0 +1,30 @@
+package leetcode_problems.stack.car_fleet;
+
+import java.util.Arrays;
+
+public class CarFleet {
+
+    public static int carFleet(int target, int[] position, int[] speed) {
+        if (position == null || position.length == 0) {
+            return 0;
+        }
+        int n = position.length;
+        int[][] distance_speed = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            distance_speed[i][0] = position[i];
+            distance_speed[i][1] = speed[i];
+        }
+        Arrays.sort(distance_speed, (a, b) -> b[0] - a[0]);
+        int res = 1;
+        double pre_arrive_time = (target - distance_speed[0][0]) * 1.0 / distance_speed[0][1];
+        for (int i = 1; i < n; i++) {
+            double cur_arrive_time = (target - distance_speed[i][0]) * 1.0 / distance_speed[i][1];
+            if (cur_arrive_time > pre_arrive_time) {
+                res++;
+                pre_arrive_time = cur_arrive_time;
+            }
+        }
+        return res;
+    }
+
+}
---
+++ src/leetcode_problems/stack/car_fleet/Main.java
@@ -0,0 +1,8 @@
+package leetcode_problems.stack.car_fleet;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = CarFleet.carFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
+        System.out.println(result);
+    }
+}
