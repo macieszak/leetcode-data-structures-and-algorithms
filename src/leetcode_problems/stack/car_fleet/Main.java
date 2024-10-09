203eaef1f68ba4fa2a3e05d52e8b9fc0ee18d2b5
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-09 22:51:55 +0200

    Leetcode 853. Car fleet, more optimal solution.

--- src/leetcode_problems/stack/car_fleet/Main.java
+++ src/leetcode_problems/stack/car_fleet/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int result = CarFleet.carFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
         System.out.println(result);
+
+        int result2 = CarFleet.carFleetBetter(10, new int[]{1, 4}, new int[]{3, 2});
+        System.out.println(result2);
     }
 }
