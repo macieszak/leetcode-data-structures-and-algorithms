6614501545355cf4ec7fe39c3763313cab1a3f00
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-10 14:38:37 +0200

    Cleaning Leetcode 853. Car fleet code.

--- src/leetcode_problems/stack/car_fleet/CarFleet.java
+++ src/leetcode_problems/stack/car_fleet/CarFleet.java
@@ -28,7 +28,7 @@ public class CarFleet {
         return res;
     }
 
-    public static int carFleetBetter(int target, int[] position, int[] speed) {
+    public static int carFleetBetterSolution(int target, int[] position, int[] speed) {
         int res = 0;
         double[] timeArr = new double[target];
         for (int i = 0; i < position.length; i++)
@@ -49,40 +49,22 @@ public class CarFleet {
     }
 
     public static int carFleetStackSolution(int target, int[] position, int[] speed) {
-        // Number of cars
         int n = position.length;
-
-        // Create pairs of position and speed
         double[][] pair = new double[n][2];
         for (int i = 0; i < n; i++) {
             pair[i][0] = position[i];
             pair[i][1] = speed[i];
         }
-
-        // Sort the pairs based on position in ascending order
         Arrays.sort(pair, (a, b) -> Double.compare(a[0], b[0]));
-
-        // Stack to keep track of car fleets
         Stack<Double> stack = new Stack<>();
-
-        // Iterate from the car closest to the target to the farthest
         for (int i = n - 1; i >= 0; i--) {
-            // Calculate the time it takes for the current car to reach the target
             double time = (target - pair[i][0]) / pair[i][1];
-
-            // If the stack is not empty and the current car will reach the target
-            // earlier or at the same time as the car at the top of the stack, it means
-            // the current car will join the fleet of the car at the top of the stack
             if (!stack.isEmpty() && time <= stack.peek()) {
-                continue; // Current car joins the fleet of the car at the top of the stack
+                continue;
             }
-
-            // If the current car does not join any existing fleet, push its time to the stack
             stack.push(time);
         }
-
-        // The size of the stack represents the number of car fleets
-        return stack.size();
+       return stack.size();
     }
 
 }
--- src/leetcode_problems/stack/car_fleet/Main.java
+++ src/leetcode_problems/stack/car_fleet/Main.java
@@ -5,7 +5,10 @@ public class Main {
         int result = CarFleet.carFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
         System.out.println(result);
 
-        int result2 = CarFleet.carFleetBetter(10, new int[]{1, 4}, new int[]{3, 2});
+        int result2 = CarFleet.carFleetBetterSolution(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
         System.out.println(result2);
+
+        int result3 = CarFleet.carFleetStackSolution(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
+        System.out.println(result3);
     }
 }
