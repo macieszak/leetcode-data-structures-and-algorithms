39c17a1a9c88aca1cf7c39548e61ce2e7d9b6c38
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-15 23:51:59 +0200

    84. Largest Rectangle in Histogram. Optimal solution.

--- src/leetcode_problems/stack/largest_rectangle/LargestRectangleInHistogram.java
+++ src/leetcode_problems/stack/largest_rectangle/LargestRectangleInHistogram.java
@@ -1,29 +1,64 @@
 package leetcode_problems.stack.largest_rectangle;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
-
-    //area = heights[i] * (endingPoint - startingPoint + 1)
-    public static int largestRectangleArea(int[] heights) {
-
-        return -1;
-    }
 
     public int largestRectangleAreaNotOptimal(int[] heights) {
         int res = 0, n = heights.length;
-
         for (int i = 0; i < n; i++) {
             int curr = heights[i];
-
-            // Traverse left while we have a greater height bar
             for (int j = i - 1; j >= 0 && heights[j] >= heights[i]; j--)
                 curr += heights[i];
-
-            // Traverse right while we have a greater height bar
             for (int j = i + 1; j < n && heights[j] >= heights[i]; j++)
                 curr += heights[i];
             res = Math.max(res, curr);
         }
         return res;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int[] previousSmallerTab = previousSmaller(heights);
+        int[] nextSmallerTab = nextSmaller(heights);
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int width = nextSmallerTab[i] - previousSmallerTab[i] - 1;
+            int area = heights[i] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+
+    public int[] nextSmaller(int[] heights) {
+        int n = heights.length;
+        int[] nextSmallerTab = new int[n];
+        Arrays.fill(nextSmallerTab, n);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[i] < heights[st.peek()]) {
+                nextSmallerTab[st.pop()] = i;
+            }
+            st.push(i);
+        }
+        return nextSmallerTab;
+    }
+
+    public int[] previousSmaller(int[] heights) {
+        int n = heights.length;
+        int[] previousSmallerTab = new int[n];
+        Arrays.fill(previousSmallerTab, -1);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[i] < heights[st.peek()]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) {
+                previousSmallerTab[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return previousSmallerTab;
     }
 
 }
--- src/leetcode_problems/stack/largest_rectangle/Main.java
+++ src/leetcode_problems/stack/largest_rectangle/Main.java
@@ -3,10 +3,9 @@ package leetcode_problems.stack.largest_rectangle;
 public class Main {
     public static void main(String[] args) {
         int[] heights = new int[] {2, 1, 5, 6, 2, 3};
-        int result = LargestRectangleInHistogram.largestRectangleArea(heights);
-        System.out.println(result);
-
         LargestRectangleInHistogram largestRectangleInHistogram = new LargestRectangleInHistogram();
+        int result = largestRectangleInHistogram.largestRectangleArea(heights);
+        System.out.println(result);
         int result2 = largestRectangleInHistogram.largestRectangleAreaNotOptimal(heights);
         System.out.println(result2);
     }
