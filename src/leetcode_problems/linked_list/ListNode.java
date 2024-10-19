8ab3eabdff8d8f4150ff6a97ebcb558897620a19
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-19 19:02:43 +0200

    206. Reverse Linked List. Iterative solution.

---
+++ src/leetcode_problems/linked_list/ListNode.java
@@ -0,0 +1,36 @@
+package leetcode_problems.linked_list;
+
+public class ListNode {
+
+    private int val;
+    private ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+}
---
+++ src/leetcode_problems/linked_list/Main.java
@@ -0,0 +1,38 @@
+package leetcode_problems.linked_list;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+
+        head.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        fourth.setNext(fifth);
+        fifth.setNext(null);
+
+        Solution reverseLinkedListSolution = new Solution();
+        ListNode newHead = reverseLinkedListSolution.reverseListIterativeSolution(head);
+        System.out.println("New head is: " + newHead.getVal() + ", next value is: " + newHead.getNext().getVal());
+    }
+}
+
+class Solution {
+
+    public ListNode reverseListIterativeSolution(ListNode head) {
+        ListNode newHead = null;
+        while (head.getNext() !=  null) {
+            ListNode next = head.getNext();
+            head.setNext(newHead);
+            newHead = head;
+            head = next;
+        }
+        head.setNext(new ListNode(-111111));
+        return head;
+    }
+
+}
